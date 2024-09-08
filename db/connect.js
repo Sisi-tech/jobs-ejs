3f4e69c1e7a76e1c2ad2e99e3eabe34655209719
Sisi Wang <sisiwang242@gmail.com>
2024-09-08 19:53:29 -0400

    week 13

---
+++ db/connect.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
+    })
+    .then(() => {
+        console.log("Connect to the DB...");
+    })
+    .catch((err) => {
+        console.error("Database connection Error: ", err);
+    })
+}
+
+module.exports = connectDB 
